0ed955752d66dc0e3478528cbf75454f3078434f
VividerAphid <trever.kress@gmail.com>
2018-06-18 02:43:49 -0400

    Added the two classic bots, WildStep and WildExpand

--- game/bot.js
+++ game/bot.js
@@ -1,4 +1,4 @@
-//Bot types, all must include at least a pickMove() method
+//Bot types, all must include at least a makeMove() method
 function randoBot(name, colour, id){
 	Player.call(this, name, colour, id, true);
 	this.name = name;
@@ -8,6 +8,7 @@ function randoBot(name, colour, id){
 randoBot.prototype = Object.create(Player.prototype);
 randoBot.prototype.constructor = randoBot;
 randoBot.prototype.makeMove = function(){
+	
 	var tar = new planeto(0,-10,-10, "#fff", 10, [-1]);
 	tar.setShield(true);
 	while (tar.getShield()){
@@ -16,5 +17,53 @@ randoBot.prototype.makeMove = function(){
 		var consList = map[pick].getConnections();
 		tar = map[consList[Math.floor(Math.random()*consList.length)]];
 	}
+	move(tar, map);
+}
+
+function wildExpand(name, colour, id){
+	Player.call(this, name, colour, id, true);
+	this.name = name;
+	this.colour = colour;
+	this.id = id;
+}
+wildExpand.prototype = Object.create(Player.prototype);
+wildExpand.prototype.constructor = wildExpand;
+wildExpand.prototype.makeMove = function(){
+	var hitList = [];
+	var owned = this.getOwned();
+	for (var r = 0; r<owned.length; r++){
+		var cons = map[owned[r]].getConnections();
+		for (var t = 0; t<cons.length; t++){
+			if(map[cons[t]].getOwner() != this.id){
+				hitList.push(map[cons[t]]);
+			}
+		}
+	}
+	var pick = Math.floor(Math.random()*hitList.length);
+	var tar = hitList[pick];
+	move(tar, map);
+}
+
+function wildStep(name, colour, id){
+	Player.call(this, name, colour, id, true);
+	this.name = name;
+	this.colour = colour;
+	this.id = id;
+}
+wildStep.prototype = Object.create(Player.prototype);
+wildStep.prototype.constructor = wildStep;
+wildStep.prototype.makeMove = function(){
+	var hitList = [];
+	var owned = this.getOwned();
+	for (var r = 0; r<owned.length; r++){
+		var cons = map[owned[r]].getConnections();
+		for (var t = 0; t<cons.length; t++){
+			if(map[cons[t]].getOwner() != this.id || map[cons[t]].getShield() != true){
+				hitList.push(map[cons[t]]);
+			}
+		}
+	}
+	var pick = Math.floor(Math.random()*hitList.length);
+	var tar = hitList[pick];
 	move(tar, map);
 }
