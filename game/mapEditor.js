2e5e61d4114ef433bf5ae6602f32813d2b2eb95e
VividerAphid <trever.kress@gmail.com>
2019-02-08 13:59:27 -0500

    Finished planeto editing controls

--- game/mapEditor.js
+++ game/mapEditor.js
@@ -19,22 +19,69 @@ function checkHit(){
 	for(r=0; r<map.length;r++){
 		if (x >= (map[r].getX() - map[r].getRadius()*2) * zoomScale && x <= (map[r].getX() + map[r].getRadius()*2) * zoomScale){
 			if (y >= (map[r].getY() - map[r].getRadius()*2) * zoomScale && y <= (map[r].getY() + map[r].getRadius()*2) * zoomScale){
-                console.log("Ding!");
-                hit = true;
-                break;
+                if(editMode == 0){
+                    map = removeAtIndex(map, r);
+                    hit = true;
+                    mapInd -= 1;
+                    break;
+                }
+                
 			}
 		}
     }
-    if (!hit){
-        map[mapInd] = new planeto(0, x, y, '#fff', 10, []);
-        mapInd += 1;
-        console.log("Added");
+    if (!hit && editMode == 0){
+        switch(placeModeSel.selectedIndex){
+            case 0:
+                map[mapInd] = new planeto(0, x, y, '#fff', 10, []);
+                mapInd += 1;
+                break;
+            case 1:
+                map[mapInd] = new planeto(0, x, y, '#fff', 10, []);
+                mapInd += 1;
+                map[mapInd] = new planeto(0, canvas.width - x, y, '#fff', 10, []);
+                mapInd += 1;
+                break;
+            case 2:
+                map[mapInd] = new planeto(0, x, y, '#fff', 10, []);
+                mapInd += 1;
+                map[mapInd] = new planeto(0, x, canvas.height - y, '#fff', 10, []);
+                mapInd += 1;
+                break;
+            case 3:
+                map[mapInd] = new planeto(0, x, y, '#fff', 10, []);
+                mapInd += 1;
+                map[mapInd] = new planeto(0, canvas.width - x, canvas.height - y, '#fff', 10, []);
+                mapInd += 1;
+                break;
+            case 4:
+                map[mapInd] = new planeto(0, x, y, '#fff', 10, []);
+                mapInd += 1;
+                map[mapInd] = new planeto(0, canvas.width - x, canvas.height - y, '#fff', 10, []);
+                mapInd += 1;
+                map[mapInd] = new planeto(0, canvas.width - x, y, '#fff', 10, []);
+                mapInd += 1;
+                map[mapInd] = new planeto(0, x, canvas.height - y, '#fff', 10, []);
+                mapInd += 1;
+                break;
+        }
+        
     }
     render();
 }
 
 function render(){
+    ctx.fillStyle = "#000";
+    ctx.fillRect(0,0,canvas.width, canvas.height);
+    
+    for (var r = 0; r < map.length; r++){
+        map[r].drawConnections(ctx, map);
+    }
     for (var r = 0; r < map.length; r++){
         map[r].drawPlaneto(ctx, map);
+    }
+}
+function printMap(){
+    for(var r=0; r<map.length; r++){
+        console.log("index " + r + map[r]);
     }
 }
