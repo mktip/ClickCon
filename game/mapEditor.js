af2c9e01c7b289c2b7431ca5a8f4eecb48a31705
VividerAphid <trever.kress@gmail.com>
2019-02-15 16:00:49 -0500

    Fixed the misbehaving connections when removing planets

--- game/mapEditor.js
+++ game/mapEditor.js
@@ -1,3 +1,24 @@
+var debug = false;
+function printMap(){
+    for(var r=0; r<map.length; r++){
+        console.log("index " + r + map[r]);
+    }
+}
+function createPlanetLabels(){
+	for (var r = 0; r<map.length; r++){
+		if(map[r].getShowing()){
+			ctx.fillStyle = "#f00";
+			ctx.strokeStyle = "#f00";
+		}
+		else{
+			ctx.fillStyle = "#fff";
+			ctx.strokeStyle = "#fff";
+		}
+		ctx.font = "20px Arial";
+		ctx.fillText(r, map[r].getX()-20, map[r].getY() - 20); //NO COORDS
+		ctx.fillText(r + " (" + map[r].getX() + "," + map[r].getY() + ")", map[r].getX()-20, map[r].getY() - 20); //SHOWS COORDS
+	}
+}
 function initPage(){
     var canX = 1000;
     var canY = 750;
@@ -9,7 +30,6 @@ function initPage(){
     var ctx = canvas.getContext("2d");
     canvas.onclick = function(){checkHit(map); return false;};
 }
-
 function checkHit(){
     var canvRect = canvas.getBoundingClientRect();
 	var x = (event.clientX - canvRect.left);
@@ -20,9 +40,8 @@ function checkHit(){
 		if (x >= (map[r].getX() - map[r].getRadius()*2) * zoomScale && x <= (map[r].getX() + map[r].getRadius()*2) * zoomScale){
 			if (y >= (map[r].getY() - map[r].getRadius()*2) * zoomScale && y <= (map[r].getY() + map[r].getRadius()*2) * zoomScale){
                 if(editMode == 0){
-                    map = removeAtIndex(map, r);
+                    removePlaneto(map[r], r);
                     hit = true;
-                    mapInd -= 1;
                     break;
                 }
                 else if(editMode == 1){
@@ -31,9 +50,11 @@ function checkHit(){
                         selected = r;
                     }
                     else{
-                        var cnt = edges.length;
-                        edges[cnt] = [selected, r];
-                        selected = -1;
+                        if(selected != r){
+                            var cnt = edges.length;
+                            edges[cnt] = [map[selected], map[r]];
+                        }  
+                        selected = -1;                  
                     }
                 }
 			}
@@ -88,10 +109,10 @@ function render(){
     
     for (var r = 0; r < edges.length; r++){
         var prev = ctx.strokeStyle;
-        var startX = map[edges[r][0]].getX();
-        var startY = map[edges[r][0]].getY();
-        var targX = map[edges[r][1]].getX();
-        var targY = map[edges[r][1]].getY();
+        var startX = edges[r][0].getX();
+        var startY = edges[r][0].getY();
+        var targX = edges[r][1].getX();
+        var targY = edges[r][1].getY();
         ctx.strokeStyle = "#999"
         ctx.lineWidth = 3;
         ctx.moveTo(startX, startY);
@@ -112,6 +133,9 @@ function render(){
 		ctx.arc(x, y, rad*2 + 8, 0, 2*Math.PI);
 		ctx.stroke();
     }
+    if(debug){
+        createPlanetLabels();
+    }
 }
 function toggleEditMode(which){
     if(which == 0){
@@ -125,8 +149,26 @@ function toggleEditMode(which){
         conBtn.style.background = "red";
     }
 }
-function printMap(){
-    for(var r=0; r<map.length; r++){
-        console.log("index " + r + map[r]);
+function removePlaneto(targ, ind){
+    //console.log(targ);
+    console.log(edges);
+    var removeList = [];
+    for(var w = 0; w < edges.length; w++){
+        if(edges[w][0] == targ || edges[w][1] == targ){
+            console.log("attempting remove");
+            console.log(targ);           
+            console.log(edges[w][0]);
+            console.log(edges[w][1]); 
+            removeList.push(w);
+        }
     }
+    for(var w = 0; w < removeList.length; w++){
+            //console.log(edges);
+            console.log(removeList[w]);
+            edges = removeAtIndex(edges, (removeList[w])-w);
+    }
+    map = removeAtIndex(map, ind);
+    console.log(edges);
+    console.log(map);
+    mapInd -= 1;
 }
