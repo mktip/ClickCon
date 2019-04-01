1b2523007e252423103dba467d2f42f514daa5ed
VividerAphid <trever.kress@gmail.com>
2019-04-01 15:58:12 -0400

    Reworking StartPage ui, stopped to update index and add updatelog page

--- game/js/graphics/ui.js
+++ game/js/graphics/ui.js
@@ -21,125 +21,141 @@ function remPlayer(event){
 }
 
 function startMenu(colList){
-	var holder = document.createElement("div");
-	var divi = document.createElement("div");
-	var mapDiv = document.createElement("div");
-	var spawnDiv = document.createElement("div");
-	var othersHolder = document.createElement("div");
-	var hostPlayer = document.createElement("div");
-	var StartBtn = document.createElement("button");
-	var AplyrBtn = document.createElement("button");
-	var BotWarBtn = document.createElement("button");
-	var hideScores = document.createElement("input");
-	var shields = document.createElement("input");
-	var roadblocks = document.createElement("input");
-	var lockMode = document.createElement("input");
-	var roundCount = document.createElement("input");
-	var mapList = document.createElement("select");
-	var spawnList = document.createElement("select");
-	var AbtnTxt = document.createTextNode("+ Bot");	
-	var strtbtntxt = document.createTextNode("Start Game");
-	var botWarBtnTxt = document.createTextNode("Bot War");
-	var mapTxt = document.createTextNode("Map: ");
-	var spawnTxt = document.createTextNode("Spawn Count: ");
-	var playerName = document.createTextNode(adjectives[Math.floor(Math.random()*adjectives.length)] + nouns[Math.floor(Math.random()*nouns.length)] + Math.floor(Math.random()*10) + Math.floor(Math.random()*10));
-	
-	var playerColour = Math.floor(Math.random()*colList.length);
-	hostPlayer.style.color = colList[playerColour];
-	colList = removeAtIndex(colList, playerColour);
-	
+	//Containers
+	addElement("holderDiv", "div", document.body);
+	addElement("settingsDiv", "div", holderDiv);
+	addElement("mapDiv", "div", settingsDiv, "Map: ");
+	addElement("spawnDiv", "div", settingsDiv, "spawn");
+	addElement("checksDiv", "div", settingsDiv, "checks");
+	addElement("playerlistDiv", "div", holderDiv, "players");
+
+	//MapList
+	addElement("mapList", "select", mapDiv);
 	var mapArr = ["Random Map", "Praise Jibbers", "Pretty Sym", "ScatterBlob", "Spiral Galaxy", "Ring of Death", "Clusters", "Heartbreak", "Converge", "Proper Spiral", "Tri Spiral", "Super Spiral", "Random Gen"];	
 	for(var x = 0; x<mapArr.length;x++){
-		var opt = document.createElement("option");
-		opt.innerHTML = mapArr[x];
-		mapList.appendChild(opt);
+		addElement("opt" + x, "option", mapList, mapArr[x]);
 	}
+
+
+	// var holder = document.createElement("div");
+	// var divi = document.createElement("div");
+	// var mapDiv = document.createElement("div");
+	// var spawnDiv = document.createElement("div");
+	// var othersHolder = document.createElement("div");
+	// var hostPlayer = document.createElement("div");
+	// var StartBtn = document.createElement("button");
+	// var AplyrBtn = document.createElement("button");
+	// var BotWarBtn = document.createElement("button");
+	// var hideScores = document.createElement("input");
+	// var shields = document.createElement("input");
+	// var roadblocks = document.createElement("input");
+	// var lockMode = document.createElement("input");
+	// var roundCount = document.createElement("input");
+	// var mapList = document.createElement("select");
+	// var spawnList = document.createElement("select");
+	// var AbtnTxt = document.createTextNode("+ Bot");	
+	// var strtbtntxt = document.createTextNode("Start Game");
+	// var botWarBtnTxt = document.createTextNode("Bot War");
+	// var mapTxt = document.createTextNode("Map: ");
+	// var spawnTxt = document.createTextNode("Spawn Count: ");
+	// var playerName = document.createTextNode(adjectives[Math.floor(Math.random()*adjectives.length)] + nouns[Math.floor(Math.random()*nouns.length)] + Math.floor(Math.random()*10) + Math.floor(Math.random()*10));
 	
-	var spawnArr = ["Random Count", "1 Spawn", "2 Spawns", "3 Spawns", "Fill"];
-	for(var x = 0; x<spawnArr.length; x++){
-		var opt = document.createElement("option");
-		opt.innerHTML = spawnArr[x];
-		spawnList.appendChild(opt);
-	}
+	// var playerColour = Math.floor(Math.random()*colList.length);
+	// hostPlayer.style.color = colList[playerColour];
+	// colList = removeAtIndex(colList, playerColour);
 	
-	StartBtn.id = "StartBtn";
-	mapList.id = "mapList";
-	spawnList.id = "spawnList";
-	divi.id = "divi";
-	AplyrBtn.id = "AplyrBtn";
-	hostPlayer.id = "hostPlayer";
-	BotWarBtn.id = "BotWarBtn";
-	holder.id = "holder";
-	hideScores.id = "hideScores";
-	shields.id = "shields";
-	roadblocks.id = "roadblocks";
-	lockMode.id = "lockMode";
-	roundCount.id = "roundCount";
+	// var mapArr = ["Random Map", "Praise Jibbers", "Pretty Sym", "ScatterBlob", "Spiral Galaxy", "Ring of Death", "Clusters", "Heartbreak", "Converge", "Proper Spiral", "Tri Spiral", "Super Spiral", "Random Gen"];	
+	// for(var x = 0; x<mapArr.length;x++){
+	// 	var opt = document.createElement("option");
+	// 	opt.innerHTML = mapArr[x];
+	// 	mapList.appendChild(opt);
+	// }
 	
-	hideScores.type = "checkbox";
-	shields.type = "checkbox";
-	roadblocks.type = "checkbox";
-	lockMode.type = "checkbox";
-	roundCount.type = "number";
-	roundCount.min = 10;
-	roundCount.max = 10000;
-	roundCount.value = 50;
+	// var spawnArr = ["Random Count", "1 Spawn", "2 Spawns", "3 Spawns", "Fill"];
+	// for(var x = 0; x<spawnArr.length; x++){
+	// 	var opt = document.createElement("option");
+	// 	opt.innerHTML = spawnArr[x];
+	// 	spawnList.appendChild(opt);
+	// }
 	
-	var hSTxt = document.createTextNode("Hide Scores");
-	var shieTxt = document.createTextNode("Random Shields");
-	var rbTxt = document.createTextNode("Random Roadblocks");
-	var forTxt = document.createTextNode("Formation Lock Mode");
-	var rdCntTxt = document.createTextNode("Round Count: ");
+	// StartBtn.id = "StartBtn";
+	// mapList.id = "mapList";
+	// spawnList.id = "spawnList";
+	// divi.id = "divi";
+	// AplyrBtn.id = "AplyrBtn";
+	// hostPlayer.id = "hostPlayer";
+	// BotWarBtn.id = "BotWarBtn";
+	// holder.id = "holder";
+	// hideScores.id = "hideScores";
+	// shields.id = "shields";
+	// roadblocks.id = "roadblocks";
+	// lockMode.id = "lockMode";
+	// roundCount.id = "roundCount";
 	
-	var hsDiv = document.createElement("div");
-	var shiDiv = document.createElement("div");
-	var rbDiv = document.createElement("div");
-	var forDiv = document.createElement("div");
-	var rndDiv = document.createElement("div");
+	// hideScores.type = "checkbox";
+	// shields.type = "checkbox";
+	// roadblocks.type = "checkbox";
+	// lockMode.type = "checkbox";
+	// roundCount.type = "number";
+	// roundCount.min = 10;
+	// roundCount.max = 10000;
+	// roundCount.value = 50;
 	
-	hsDiv.appendChild(hSTxt);
-	hsDiv.appendChild(hideScores);
-	shiDiv.appendChild(shieTxt);
-	shiDiv.appendChild(shields);
-	rbDiv.appendChild(rbTxt);
-	rbDiv.appendChild(roadblocks);
-	forDiv.appendChild(forTxt);
-	forDiv.appendChild(lockMode);
-	rndDiv.appendChild(rdCntTxt);
-	rndDiv.appendChild(roundCount);
+	// var hSTxt = document.createTextNode("Hide Scores");
+	// var shieTxt = document.createTextNode("Random Shields");
+	// var rbTxt = document.createTextNode("Random Roadblocks");
+	// var forTxt = document.createTextNode("Formation Lock Mode");
+	// var rdCntTxt = document.createTextNode("Round Count: ");
 	
-	AplyrBtn.appendChild(AbtnTxt);
-	hostPlayer.appendChild(playerName);
-	StartBtn.appendChild(strtbtntxt);
-	BotWarBtn.appendChild(botWarBtnTxt);
-	divi.appendChild(hostPlayer);
-	mapDiv.appendChild(mapTxt);
-	mapDiv.appendChild(mapList);
-	spawnDiv.appendChild(spawnTxt);
-	spawnDiv.appendChild(spawnList);
-	othersHolder.appendChild(shiDiv);
-	othersHolder.appendChild(rbDiv);
-	othersHolder.appendChild(forDiv);
-	othersHolder.appendChild(hsDiv);
+	// var hsDiv = document.createElement("div");
+	// var shiDiv = document.createElement("div");
+	// var rbDiv = document.createElement("div");
+	// var forDiv = document.createElement("div");
+	// var rndDiv = document.createElement("div");
+	
+	// hsDiv.appendChild(hSTxt);
+	// hsDiv.appendChild(hideScores);
+	// shiDiv.appendChild(shieTxt);
+	// shiDiv.appendChild(shields);
+	// rbDiv.appendChild(rbTxt);
+	// rbDiv.appendChild(roadblocks);
+	// forDiv.appendChild(forTxt);
+	// forDiv.appendChild(lockMode);
+	// rndDiv.appendChild(rdCntTxt);
+	// rndDiv.appendChild(roundCount);
+	
+	// AplyrBtn.appendChild(AbtnTxt);
+	// hostPlayer.appendChild(playerName);
+	// StartBtn.appendChild(strtbtntxt);
+	// BotWarBtn.appendChild(botWarBtnTxt);
+	// divi.appendChild(hostPlayer);
+	// mapDiv.appendChild(mapTxt);
+	// mapDiv.appendChild(mapList);
+	// spawnDiv.appendChild(spawnTxt);
+	// spawnDiv.appendChild(spawnList);
+	// othersHolder.appendChild(shiDiv);
+	// othersHolder.appendChild(rbDiv);
+	// othersHolder.appendChild(forDiv);
+	// othersHolder.appendChild(hsDiv);
 
 	
-	AplyrBtn.addEventListener("click", function(event){event.preventDefault(); addPlayer(divi, document.createElement("div"));});
-	StartBtn.addEventListener("click", function(event){event.preventDefault(); gameStart();});
-	BotWarBtn.addEventListener("click", function(event){event.preventDefault(); botwar = true; gameStart();});
+	// AplyrBtn.addEventListener("click", function(event){event.preventDefault(); addPlayer(divi, document.createElement("div"));});
+	// StartBtn.addEventListener("click", function(event){event.preventDefault(); gameStart();});
+	// BotWarBtn.addEventListener("click", function(event){event.preventDefault(); botwar = true; gameStart();});
 	
-	holder.appendChild(StartBtn);
-	holder.appendChild(BotWarBtn);
-	holder.appendChild(mapDiv);
-	holder.appendChild(spawnDiv);
-	holder.appendChild(rndDiv);
-	holder.appendChild(othersHolder);
-	holder.appendChild(divi);
-	holder.appendChild(AplyrBtn);
-	document.body.appendChild(holder);
+	// holder.appendChild(StartBtn);
+	// holder.appendChild(BotWarBtn);
+	// holder.appendChild(mapDiv);
+	// holder.appendChild(spawnDiv);
+	// holder.appendChild(rndDiv);
+	// holder.appendChild(othersHolder);
+	// holder.appendChild(divi);
+	// holder.appendChild(AplyrBtn);
+	// document.body.appendChild(holder);
 	
-	addPlayer(divi, document.createElement("div"), colList);
-	addPlayer(divi, document.createElement("div"), colList);
-	addPlayer(divi, document.createElement("div"), colList);
+	// addPlayer(divi, document.createElement("div"), colList);
+	// addPlayer(divi, document.createElement("div"), colList);
+	// addPlayer(divi, document.createElement("div"), colList);
 }
 
 function fillControlBox(){
