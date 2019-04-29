4de8d4ae39f9fd3f9adddcbfe55a4f26acd9c91a
VividerAphid <trever.kress@gmail.com>
2019-04-29 14:30:08 -0400

    Split player code off into a separate file

--- game/js/game.js
+++ game/js/game.js
@@ -1,29 +1,3 @@
-function Player(name, colour, id, bot){
-	this.name = name;
-	this.colour = colour;
-	this.id = id;
-	this.isBot = bot;
-	this.score;
-}
-Player.prototype.getOwned = function(){
-	var list = [];
-	for(var r = 0; r<map.length; r++){
-		if (map[r].getOwner() == this.id){
-			list.push(r);
-		}
-	}
-	return list;
-}
-Player.prototype.getColour = function(){
-	return this.colour;
-}
-Player.prototype.getID = function(){
-	return this.id;
-}
-Player.prototype.getScore = function(){
-	return this.score;
-}
-
 var debug = false;
 var playing = false;
 var fog = false;
