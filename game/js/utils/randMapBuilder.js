a7f68380f84920843587900db20b2d00840da44d
VividerAphid <trever.kress@gmail.com>
2019-09-03 17:39:12 -0400

    More maps, made rings, working on 'clusters'

--- game/js/utils/randMapBuilder.js
+++ game/js/utils/randMapBuilder.js
@@ -59,22 +59,32 @@ function getCoords(w, h, density, padding, r){
     var plans = [];
 
 	//plans = generateChunk(w,h, density, padding, r);
-	plans = midSmash(w,h,density, padding, r);
-	var type = Math.floor(Math.random() * 4);
-	switch(type){
-		case 0:
-			generateChunk(w,h,density, padding, r);
-			break;
-		case 1:
-			bottleNeck(h,h,density,padding, r);
-			break;
-		case 2:
-			circle(h,h,density,padding, r);
-			break;
-		case 3:
-			midSmash(h,h,density,padding,r);
-			break;
-	}
+	//plans = circle(w,h,density,padding,r);
+	plans = clusters(w,h,density, padding, r, -1, 500);
+	// var type = Math.floor(Math.random() * 5);
+	// switch(type){
+	// 	case 0:
+	// 		console.log("chunk!");
+	// 		plans = generateChunk(w, h, density, padding, r);
+	// 		break;
+	// 	case 1:
+	// 		console.log("bottle!");
+	// 		plans = bottleNeck(h,h,density,padding, r);
+	// 		break;
+	// 	case 2:
+	// 		console.log("circle!");
+	// 		plans = circle(h,h,density,padding, r);
+	// 		break;
+	// 	case 3:
+	// 		console.log("smash!");
+	// 		plans = midSmash(h,h,density,padding,r);
+	// 		break;
+	// 	case 4:
+	// 		console.log("ring!");
+	// 		plans = ring(h,h,density,padding,r);
+	// 		break;
+	// }
+	//console.log(plans.length);
     return plans;
 }
 
@@ -196,5 +206,81 @@ function midSmash(w, h, density, padding, r){
 }
 
 function ring(w, h, density, padding, r){
+	var cords = [];
+	count = Math.floor(density*(w-2*r)*(h-2*r)/(r*r*Math.PI));
+	var outRad = h/2 - padding*2;
+	var inRad = outRad / 2;
+	var cX = w/2;
+	var cY = h/2;
+	for(var t = 0; t<count; t++){
+		var x = Math.floor((Math.random()*(w-2*padding))+ padding);
+		var y = Math.floor((Math.random()*(h-2*padding))+ padding);
+		while((x - cX)*(x - cX) + (y - cY)*(y - cY) > outRad*outRad || (x - cX)*(x - cX) + (y - cY)*(y - cY) < inRad*inRad){
+			var safe = false;
+			while (!safe) {
+				var x = Math.floor((Math.random()*(w-2*padding))+ padding);
+				var y = Math.floor((Math.random()*(h-2*padding))+ padding);
+					var p = [x,y];
+					safe = true;
+					for (var j=0; j<cords.length; j+=1) {
+						var q = cords[j];
+						if ((p[0]-q[0])*(p[0]-q[0])+(p[1]-q[1])*(p[1]-q[1])<r*r) {
+							safe = false;
+							break;
+						}
+					}
+			}
+		}
+		cords.push([x,y]);	
+	}
+	return cords;
+}
 
+function clusters(w,h,density,padding,r,type, size){
+	var clustCoords = [];
+	clustCoords = generateChunk(w,h,.7,padding, size);
+	var coords = [];
+	for(var x = 0; x<clustCoords.length; x++){
+		var temp = [];
+		switch(type){
+			case -1:
+				var pick = Math.floor(Math.random()*4);
+				switch(pick){
+					case 0:
+						temp = generateChunk(size*2,size*2,density,padding,r);
+						break;
+					case 1:
+						temp = ring(size*2,size*2,density,padding,r);
+						break;
+					case 2:
+						temp = ring(size*2,size*2,density,padding,r);
+						break;
+					case 3:
+						temp = bottleNeck(size*2,size*2,density,padding,r);
+						break;
+				}
+				break;
+			case 0:
+				temp = generateChunk(size*2,size*2,density,padding,r);
+				break;
+			case 1:
+				temp = ring(size*2,size*2,density,padding,r);
+				break;
+			case 2:
+				temp = ring(size*2,size*2,density,padding,r);
+				break;
+			case 3:
+				temp = bottleNeck(size*2,size*2,density,padding,r);
+				break;
+		}
+		for(var zz=0; zz<temp.length; zz++){
+			temp[zz][0]+= clustCoords[x][0];
+			temp[zz][1]+= clustCoords[x][1];
+		}
+		//console.log(temp);
+		coords = coords.concat(temp);
+	}
+	//console.log(clustCoords);
+	//console.log(coords);
+	return coords;
 }
