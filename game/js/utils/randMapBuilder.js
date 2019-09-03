6dfd324dfcfcf94e3f847ef155e5ff36147a82ed
VividerAphid <trever.kress@gmail.com>
2019-09-02 23:03:59 -0400

    New map types! Temp mini addition to throw in to the working game

--- game/js/utils/randMapBuilder.js
+++ game/js/utils/randMapBuilder.js
@@ -59,7 +59,22 @@ function getCoords(w, h, density, padding, r){
     var plans = [];
 
 	//plans = generateChunk(w,h, density, padding, r);
-	plans = bottleNeck(w,h,density, padding, r);
+	plans = midSmash(w,h,density, padding, r);
+	var type = Math.floor(Math.random() * 4);
+	switch(type){
+		case 0:
+			generateChunk(w,h,density, padding, r);
+			break;
+		case 1:
+			bottleNeck(h,h,density,padding, r);
+			break;
+		case 2:
+			circle(h,h,density,padding, r);
+			break;
+		case 3:
+			midSmash(h,h,density,padding,r);
+			break;
+	}
     return plans;
 }
 
@@ -116,5 +131,70 @@ function bottleNeck(w, h, density, padding, r){
 }
 
 function circle(w, h, density, padding, r){
-	
+	var cords = [];
+	count = Math.floor(density*(w-2*r)*(h-2*r)/(r*r*Math.PI));
+	var rad = h/2 - (padding*2);
+	var cX = w/2;
+	var cY = h/2;
+	cords.push([cX, cY]);
+	for(var t = 0; t<count; t++){
+		var x = Math.floor((Math.random()*(w-2*padding))+ padding);
+		var y = Math.floor((Math.random()*(h-2*padding))+ padding);
+		while((x - cX)*(x - cX) + (y - cY)*(y - cY) > rad*rad){
+			var safe = false;
+			while (!safe) {
+				var x = Math.floor((Math.random()*(w-2*padding))+ padding);
+				var y = Math.floor((Math.random()*(h-2*padding))+ padding);
+				var p = [x,y];
+				safe = true;
+				for (var j=0; j<cords.length; j+=1) {
+					var q = cords[j];
+					if ((p[0]-q[0])*(p[0]-q[0])+(p[1]-q[1])*(p[1]-q[1])<r*r) {
+						safe = false;
+						break;
+					}
+				}
+			}
+		}
+		cords.push([x,y]);	
+	}
+	return cords;
+}
+
+function midSmash(w, h, density, padding, r){
+	var cords = [];
+	count = Math.floor(density*(w-2*r)*(h-2*r)/(r*r*Math.PI));
+	var outRad = h/2 - padding*2;
+	var inRad = outRad / 3;
+	var cX = w/2;
+	var cY = h/2;
+	cords.push([cX, cY]);
+	for(var t = 0; t<count; t++){
+		var x = Math.floor((Math.random()*(w-2*padding))+ padding);
+		var y = Math.floor((Math.random()*(h-2*padding))+ padding);
+		while((x - cX)*(x - cX) + (y - cY)*(y - cY) > outRad*outRad){
+			var safe = false;
+			while (!safe) {
+				var x = Math.floor((Math.random()*(w-2*padding))+ padding);
+				var y = Math.floor((Math.random()*(h-2*padding))+ padding);
+				if((x - cX)*(x - cX) + (y - cY)*(y - cY) < inRad*inRad){
+					var p = [x,y];
+					safe = true;
+					for (var j=0; j<cords.length; j+=1) {
+						var q = cords[j];
+						if ((p[0]-q[0])*(p[0]-q[0])+(p[1]-q[1])*(p[1]-q[1])<r*r) {
+							safe = false;
+							break;
+						}
+					}
+				}
+			}
+		}
+		cords.push([x,y]);	
+	}
+	return cords;
+}
+
+function ring(w, h, density, padding, r){
+
 }
