8ee580b65f70d3316e748e2b2ea31a0b9189e9c3
VividerAphid <trever.kress@gmail.com>
2020-01-23 22:28:44 -0500

    More items added, tinkered with playerblob css

--- rewrite/js/graphics/ui.js
+++ rewrite/js/graphics/ui.js
@@ -26,29 +26,63 @@ function startMenu(cols){
     //Player Div Holder
     addElement("leftHolder", "div", playerHolder);
     addElement("rightHolder", "div", playerHolder);
-    for(var r = 0; r < 4 ; r++){
-        addPlayerBlob(r, false, colors, false);
+    for(var r = 0; r < 16 ; r++){
+        if(r%2 == 0){
+            addPlayerBlob(r, false, colors, true);
+        }
+        else{
+            addPlayerBlob(r, false, colors, false);
+        }
+
         colors = removeAtIndex(colors, 0);
     }
-    //Settings Holder
+    toggleBlobType(blob0, true, 0);
 
+    //Settings Holder
+    addElement("mapDrp", "select", settingsMaster);
+    var mapArr = ["Random Map", "Praise Jibbers", "Pretty Sym", "ScatterBlob", "Spiral Galaxy", "Ring of Death", "Clusters", "Heartbreak", "Converge", "Proper Spiral", "Tri Spiral", "Super Spiral", "Random Gen"];	
+	for(var x = 0; x<mapArr.length;x++){
+		addElement("opt" + x, "option", mapDrp, mapArr[x]);
+    }
+    
+    addElement("spawnDrp", "select", settingsMaster);
+	var spawnArr = ["Random Count", "1 Spawn", "2 Spawns", "3 Spawns", "Fill"];
+	for(var x = 0; x<spawnArr.length; x++){
+		addElement("opt"+ x, "option", spawnDrp, spawnArr[x]);
+    }
+    
+    addElement("rndLbl", "label", settingsMaster, "<br>Round Count: ");
+	addElement("roundCount", "input", settingsMaster);
+	roundCount.type = "number";
+	roundCount.value = 150;
+    rndLbl.htmlFor = 'roundCount';
+    
     //Map preview Holder
 
 }
 
 function addPlayerBlob(blobNum, pType, cols, side){
-    addElement(("blob"+blobNum), "div", playerHolder, "Some basic text for color testing <br>");
+    if(side){
+        addElement(("blob"+blobNum), "div", leftHolder);
+    }
+    else{
+        addElement(("blob"+blobNum), "div", rightHolder);
+    }
     var curr = document.getElementById("blob"+blobNum);
+    addElement(("pDiv" + blobNum), "div", curr, "Player!");
+    toggleBlobType(curr, pType, blobNum);
     curr.className = "playerBlob";
     curr.style.background = cols[0];
     curr.style.border = cols[0];
 }
 
-function toggleBlobType(blob, pType){
+function toggleBlobType(blob, pType, num){
     if(pType){
-
+        document.getElementById(("bDiv" + num)).remove();
+        addElement(("pDiv" + num), "div", blob, "Player!");
     }
     else{
-
+        document.getElementById(("pDiv" + num)).remove();
+        addElement(("bDiv" + num), "div", blob, "Bot!");
     }
 }
