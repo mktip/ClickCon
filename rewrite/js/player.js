d4e52588a8c046a5eee83e0d9e32c790cc98b331
VividerAphid <trever.kress@gmail.com>
2020-08-24 21:42:27 -0400

    Finished player class, testing for tweaks

--- rewrite/js/player.js
+++ rewrite/js/player.js
@@ -1,40 +1,57 @@
-function player(){
-    //id
-    //tId
-    //name
-    //colour
-    //tColour
-    //cInverse
-    //score
-    //owned
-    //isBot
+function player(id, tId, name, colour, tColour, cInverse, tChar, pChar, isBot){
+    this.id = id;
+    this.tId = tId;
+    this.name = name;
+    this.colour = colour;
+    this.tColour = tColour;
+    this.cInverse = cInverse;
+    this.tChar = tChar;
+    this.pChar = pChar;
+    this.score = 0;
+    this.isBot = isBot;
 
 }
 
 player.prototype.getOwned = function(){
-
+    var list = [];
+	for(var r = 0; r<map.length; r++){
+		if (map[r].getOwner() == this.id){
+			list.push(r);
+		}
+	}
+	return list;
 }
 
 player.prototype.getScore = function(){
-
+    return this.score;
 }
 
 player.prototype.getColour = function(){
+    return this.colour;
+}
 
+player.prototype.getInverse = function(){
+    return this.cInverse;
+}
+
+player.prototype.getChars = function(){
+    return [this.tChar, this.pChar];
 }
 
 player.prototype.getTeamColour = function(){
-
+    return this.tColour;
 }
 
 player.prototype.getId = function(){
-
+    return this.id;
 }
 
-player.prototype.getTeam = function(){
-
+player.prototype.getTeamId = function(){
+    return this.tId;
 }
 
-player.prototype.setTeam = function(){
-    
+player.prototype.setTeam = function(tId, tCol, tChar){
+    this.tId = tId;
+    this.tColour = tCol;
+    this.tChar = tChar;
 }
--- rewrite/js/runner.js
+++ rewrite/js/runner.js
@@ -1,13 +1,16 @@
 (function(){
     var colList = [["#ff0000","A","#0000ff"], ["#00ff00","B","#ff00ff"], ["#0000ff","C","#ffff00"], ["#ffff00","D","#0000ff"], ["#ff00ff","E","#00ff00"], ["#00cccc","F","#ff3333"], ["#e65c00","G","#19a3ff"], ["#2e5cb8","H","#d1a347"], ["#800080","I","#7fff7f"], ["#663300","J","#99ccff"], ["#ff8080","K","#007f7f"], ["#00802b","L","#ff7fd4"], ["#008080","M","#ff7f7f"], ["#800000","N","#7fffff"], ["#666699","O","#999966"], ["#cc9900","P","#3366ff"]];
     startMenu(colList);
-    var gameMap = praiseJibbers();
+    var gameMap = mapConverter(testerMap());
 
-     gameMap[5].lockLife = 5;
-     gameMap[3].cInverse = "#fff";
-     gameMap[3].lockLife = 5;
+    var tstpl = new player(1, 1, "Mins-1", colList[8][0], colList[8][0], colList[8][2], colList[8][1],colList[8][1], false);
+
+    console.log(tstpl);
+
+    //tId, tCol, oId, oCol, iCol, oChar, tChar
+    gameMap[1].setOwner(tstpl.getTeamId(), tstpl.getTeamColour(), tstpl.getId(), tstpl.getColour(), tstpl.getInverse(), tstpl.getChars()[1], tstpl.getChars()[0]);
 
     var g = mPreview.getContext("2d");
-    g.scale(.15, .15);
+    //g.scale(.15, .15);
     render(g, gameMap, {fog: false, colourblind: true, debug: false});
 })();
